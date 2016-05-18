375de3f235c7b90e192967242103e69a3d7205dd
Sergio Soro <ssm.fuentes@gmail.com>
2016-05-19 00:04:49 +0200

    add docker-compose

--- core/config.js
+++ core/config.js
@@ -3,12 +3,14 @@ module.exports = {
     {
         production: "mongodb://localhost/wallapoints-prod",
         development: "mongodb://localhost/wallapoints-dev",
-        test: "mongodb://localhost/wallapoints-test"
+        test: "mongodb://localhost/wallapoints-test",
+        docker: "mongodb://mongo/wallapoints-prod"
 
     },
     port:{
         production: 8888,
         development: 8888,
+        docker: 8888,
         test: 8889
     },
     deploy:{
